9ce4c5cd91873229605d93f79c8900a4de3314ce
hugo <hugo193b16@gmail.com>
2026-01-19 14:22:27 +0100

    password manager for web_security pages

--- js/gamesJS/inGameJs/web_security_game.js
+++ js/gamesJS/inGameJs/web_security_game.js
@@ -1,1 +1,11 @@
-window.addEventListener('load', signIn());
+window.addEventListener("load", signIn());
+
+document.addEventListener("DOMContentLoaded", () => {
+    const location = window.location.href.split("/");
+  location.forEach((element) => {
+    if (element.includes("web")) {
+        const page = element.split(".")
+        console.log(page[0]);
+    }
+  });
+});
