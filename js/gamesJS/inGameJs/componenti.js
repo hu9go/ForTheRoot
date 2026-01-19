dc589ccbd49a845d60cffa035180a4b91163418a
Malaka-Gonawala <malakagonawala123@gmail.com>
2026-01-19 20:43:16 +0100

    progress in cryptography mode

--- js/gamesJS/inGameJs/componenti.js
+++ js/gamesJS/inGameJs/componenti.js
@@ -2,32 +2,38 @@ const main = document.querySelector("main");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 
-const usernameInput = document.getElementById("#username");
-const passwordInput = document.getElementById("#password");
-const signInForm = document.getElementById(".sign-in-form");
-function signIn() {
-  main.innerHTML = `
-<form class="sign-in-form">
-      <div class="input-group username">
-        <input type="text" name="username" id="username" required/>
-        <label for="username">Username</label>
-      </div>
-      <div class="input-group password">
-        <input type="password" name="password" id="password" required/>
-        <label for="username">Password</label>
-      </div>
-      <div class="btns">
-        <input type="submit" value="Sign in" id="sign-in"/>
-        <input type="reset" value="Cancel" id="cancel"/>
-      </div>
+var signInFormConatiner;
+function signIn(onSubmit) {
+  signInFormConatiner = document.createElement("div");
+  signInFormConatiner.classList.add("sign-in-form-Conatiner");
+  signInFormConatiner.innerHTML = `<form class="sign-in-form">
+    <div class="input-group username">
+    <input type="text" name="username" id="username" required/>
+    <label for="username">Username</label>
+    </div>
+    <div class="input-group password">
+    <input type="password" name="password" id="password" required/>
+    <label for="username">Password</label>
+    </div>
+    <div class="btns">
+    <input type="submit" value="Sign in" id="sign-in"/>
+    <input type="reset" value="Cancel" id="cancel"/>
+    </div>
     </form>`;
+  main.appendChild(signInFormConatiner);
 
-  document.body.style.backgroundColor = "#2e2e2e";
+  signInFormConatiner.style.opacity = "1";
 
-  signInForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(usernameInput.value);
-  });
+  const signInForm = document.querySelector(".sign-in-form");
+  const usernameInput = document.querySelector("#username");
+  const passwordInput = document.querySelector("#password");
+
+  if (signInForm) {
+    signInForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      onSubmit(usernameInput.value, passwordInput.value);
+    });
+  }
 }
 
 function getLocation() {
--- js/gamesJS/inGameJs/web_security_game.js
+++ js/gamesJS/inGameJs/web_security_game.js
@@ -1,7 +1,30 @@
-window.addEventListener("load", signIn());
+let credenziali = [];
+let usernameRequired = getLocation();
+window.addEventListener("load", function () {
+  signIn((username, password) => {
+    credenziali = [];
+    credenziali.push([username, password]);
+    console.log(credenziali);
+    console.log(credenziali[0], usernameRequired);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const usernameRequired = getLocation();
-  // const cancelButton = document.getElementById("#cancel");
-  // ciao
+    if (
+      credenziali[0][0] === usernameRequired &&
+      credenziali[0][1] ===
+        usernamePassword[0].find((user) => user.username === usernameRequired)
+          .password
+    ) {
+      signInFormConatiner.style.opacity = "0";
+    } else {
+      signInFormConatiner.style.opacity = "1";
+    }
+  });
+  if (usernameRequired === "web1") {
+    document.addEventListener("contextmenu", disableRightclick);
+  } else {
+    document.removeEventListener("contextmenu", disableRightclick);
+  }
+
+  function disableRightclick(event) {
+    event.preventDefault();
+  }
 });
