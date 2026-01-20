cd96c2cb34c675fd2495c6d88265da62e7d4a007
Malaka-Gonawala <malakagonawala123@gmail.com>
2026-01-20 16:34:04 +0100

    js improvements

--- js/gamesJS/inGameJs/web_security_game.js
+++ js/gamesJS/inGameJs/web_security_game.js
@@ -1,120 +1,103 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  let credenziali = [];
-  let usernameRequired = getLocation();
-  window.addEventListener("load", function () {
-    signIn((username, password) => {
-      credenziali = [];
-      credenziali.push([username, password]);
-      console.log(credenziali);
-      console.log(credenziali[0], usernameRequired);
-
-      if (
-        credenziali[0][0] === usernameRequired &&
-        credenziali[0][1] ===
-          usernamePassword[0].find((user) => user.username === usernameRequired)
-            .password
-      ) {
-        signInFormConatiner.style.opacity = "0";
-        signInFormConatiner.style.pointerEvents = "none";
-      } else {
-        signInFormConatiner.style.opacity = "1";
-        signInFormConatiner.style.pointerEvents = "auto";
-      }
-    });
-    if (usernameRequired === "web1") {
-      document.addEventListener("contextmenu", disableRightclick);
-    } else {
-      document.removeEventListener("contextmenu", disableRightclick);
-    }
-
-    function disableRightclick(event) {
-      event.preventDefault();
-    }
-  });
-
-  function getLocation() {
-    const location = window.location.href.split("/");
-    let usernameRequired = "";
-    location.forEach((element) => {
-      if (element.includes("web")) {
-        const page = element.split(".");
-        usernameRequired = page[0];
-      }
-    });
-    return usernameRequired;
-  }
-});
-*/
-
 document.addEventListener("DOMContentLoaded", function () {
-  let credenziali = [];
-  let usernameRequired = getLocation();
-  window.addEventListener("load", function () {
-    
-    signIn((username, password) => {
-      credenziali = [];
-      credenziali.push([username, password]);
-      console.log(credenziali);
-      console.log(credenziali[0], usernameRequired);
+    const main = document.querySelector("main");
+    const signInFormConatiner = document.querySelector(
+        ".sign-in-form-Conatiner",
+    );
+    let credenziali = [];
+    let usernameRequired = getLocation();
+    window.onload(() => {
+        if (usernameRequired === "web1") {
+            window.location.replace("kicked.html");
+            window.open("web1.html", "_blank");
+        }
+    });
 
-      if (
-        credenziali[0][0] === usernameRequired &&
-        credenziali[0][1] ===
-          usernamePassword[0].find((user) => user.username === usernameRequired)
-            .password
-      ) {
-        signInFormConatiner.style.opacity = "0";
-        signInFormConatiner.style.pointerEvents = "none";
-        getGameData(usernameRequired);
-      } else {
-        signInFormConatiner.style.opacity = "1";
-        signInFormConatiner.style.pointerEvents = "auto";
-      }
+    main.innerHTML = ""; // Clear main content on load
+    signIn((username, password) => {
+        credenziali = [];
+        credenziali.push([username, password]);
+        console.log(credenziali);
+        console.log(credenziali[0], usernameRequired);
+
+        if (
+            credenziali[0][0] === usernameRequired &&
+            credenziali[0][1] ===
+                usernamePassword[0].find(
+                    (user) => user.username === usernameRequired,
+                ).password
+        ) {
+            signInFormConatiner.style.opacity = "0";
+            signInFormConatiner.style.pointerEvents = "none";
+            getGameData(usernameRequired);
+        } else {
+            signInFormConatiner.style.opacity = "1";
+            signInFormConatiner.style.pointerEvents = "auto";
+        }
     });
     if (usernameRequired === "web1") {
-      document.addEventListener("contextmenu", disableRightclick);
+        document.addEventListener("contextmenu", disableRightclick);
     } else {
-      document.removeEventListener("contextmenu", disableRightclick);
+        document.removeEventListener("contextmenu", disableRightclick);
     }
 
     function disableRightclick(event) {
-      event.preventDefault();
+        event.preventDefault();
     }
-  });
 
-  function getLocation() {
-    const location = window.location.href.split("/");
-    let usernameRequired = "";
-    location.forEach((element) => {
-      if (element.includes("web")) {
-        const page = element.split(".");
-        usernameRequired = page[0];
-      }
-    });
-    return usernameRequired;
-  }
+    function getLocation() {
+        const location = window.location.href.split("/");
+        let usernameRequired = "";
+        location.forEach((element) => {
+            if (element.includes("web")) {
+                const page = element.split(".");
+                usernameRequired = page[0];
+            }
+        });
+        return usernameRequired;
+    }
 
-  function getGameData(usernameRequired) {
-    switch   (usernameRequired) {
-      case "web0":
-        setupWeb0();
-        break;
-      case "web1":
-        setupWeb1();
-        break;
-      case "web2":
-        setupWeb2();
-        break;
-      case "web3":
-        setupWeb3();
-        break;
-      default:
-        console.log("No game data found for this user.");
-    } 
-  }
+    function getGameData(usernameRequired) {
+        switch (usernameRequired) {
+            case "web0":
+                setupWeb0();
+                break;
+            case "web1":
+                setupWeb1();
+                break;
+            case "web2":
+                setupWeb2();
+                break;
+            case "web3":
+                setupWeb3();
+                break;
+            default:
+                main.innerHTML = `<p>Pagina non trovata</p>`;
+        }
+    }
 
-  setupWeb0 = () => {
-    console.log("Setting up Web0 game data...");
-    // Add specific setup code for Web0 here
-  }
+    setupWeb0 = () => {
+        main.innerHTML = `<p>
+                In questa pagina trovi la password per /web1. <br /><br />
+                Per cambiare livello, cambia l'URL della pagina (/web0 ->
+                /web1).
+            </p>
+            <!-- complimenti! la soluzione è ispezionare la pagina per leggere il file sorgente. -->
+            <!-- user: web1 -->
+            <!-- password: A1#b4C9*e2F0g!H8 -->`;
+    };
+    setupWeb1 = () => {
+        main.innerHTML = `<p>
+                qui trovi la password per la prossima pagina ma il tasto destro
+                è disattivato.
+            </p>
+            <!-- complimenti! la soluzione è premere F12. -->
+            <!-- user: web2 -->
+            <!-- password: 7D&3a9$B1e5@fC2R -->`;
+    };
+    setupWeb2 = () => {
+        main.innerHTML = `<p>DA LAVORARE</p>`;
+    };
+    setupWeb3 = () => {
+        main.innerHTML = `<p>approfindimenti</p>`;
+    };
 });
