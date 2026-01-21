0d3b891f4f877f3c7f67e6d76f94a89d33f2b4df
Malaka Gonawala <malakagonawala123@gmail.com>
2026-01-21 10:31:55 +0100

    finalized project

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 console.log("send help");
-console.log("world");
+
+const tornaSu = document.querySelector(".tornasu");
+function toggleTornaSu() {
+  if (window.scrollY > 300) {
+    tornaSu.classList.add("show");
+    tornaSu.classList.remove("hide");
+  } else {
+    tornaSu.classList.add("hide");
+    tornaSu.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", toggleTornaSu);
+window.addEventListener("load", toggleTornaSu);
